cf1c3a47c81137ea9e7afd7fbae714c64e762cab
tomchentw <tomchentw@users.noreply.github.com>
2021-06-03 08:15:28 +0000

    deploy: 50540f7adcc02f1f06065921777d8dd85562e22a

--- _next/static/0Jc82KdOKcDq79dxsS89t/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6f782a0d15159afcfb7a.js"],"/_error":["static/chunks/pages/_error-3e2b207bac140df45f97.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/JKi3iiyamReR9LxBRxVyM/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcompany\u002F[encodedSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
