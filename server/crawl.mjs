1cad73626b4058bdc10bb584ce81160b7536639f
Tom Chen <developer@tomchentw.com>
2021-06-03 13:10:46 +0800

    feat(server/crawl): parse atom feed and store as custom json

--- server/crawl.mjs
+++ server/crawl.mjs
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { parseFeed } from "htmlparser2";
 import createFetch from "@vercel/fetch";
 
 const fetch = createFetch();
@@ -11,19 +12,22 @@ const RAW_DIR = path.join(DIRNAME, `../public/data/raw`);
 const INTERVAL = 5 * 60 * 1000; // 5 minute
 const URL_LIST = [
   {
-    pathname: `1q5Y5hWgQJPfIk9VhSYnSZ3ENZz9UIF03NzSusgpg6F4.csv`,
-    url: `https://docs.google.com/spreadsheets/d/1q5Y5hWgQJPfIk9VhSYnSZ3ENZz9UIF03NzSusgpg6F4/export?gid\u003d8838146\u0026format\u003dcsv`,
+    pathname: `1q5Y5hWgQJPfIk9VhSYnSZ3ENZz9UIF03NzSusgpg6F4.json`,
+    url: `https://spreadsheets.google.com/feeds/cells/1q5Y5hWgQJPfIk9VhSYnSZ3ENZz9UIF03NzSusgpg6F4/o59fx4/public/basic`,
+    transform: atomFeedToCustomJson,
   },
 ];
 
 export default async function crawl() {
   await fs.promises.mkdir(RAW_DIR, { recursive: true });
   await Promise.all(
-    URL_LIST.map(async ({ pathname, url }) => {
+    URL_LIST.map(async ({ pathname, url, transform }) => {
       while (true) {
         const dataPath = path.join(RAW_DIR, pathname);
         const [text, prevText] = await Promise.all([
-          fetch(url).then((r) => r.text()),
+          fetch(url)
+            .then((r) => r.text())
+            .then(transform),
           fs.promises.readFile(dataPath, "utf-8").catch((error) => {
             if ("ENOENT" !== error.code) {
               throw error;
@@ -39,4 +43,27 @@ export default async function crawl() {
       }
     })
   );
+}
+
+function atomFeedToCustomJson(string) {
+  const dom = parseFeed(string);
+  const { list } = dom.items.reduce(
+    (acc, { title, description }) => {
+      const [_, rowNumber] = title.match(/^\S(\d+)$/);
+      const index = parseInt(rowNumber, 10) - 1;
+      if (index === acc.currentIndex + 1) {
+        acc.list.push(`  [${acc.current.join(", ")}]`);
+        acc.current = [];
+        acc.currentIndex = acc.list.length;
+      }
+      acc.current.push(JSON.stringify(description || ""));
+      return acc;
+    },
+    {
+      currentIndex: 0,
+      current: [],
+      list: [],
+    }
+  );
+  return `[\n${list.join(",\n")}\n]`;
 }
