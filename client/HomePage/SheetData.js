4dd826a32feb932cdca541ca0f0d53fc3839ac5c
Tom Chen <developer@tomchentw.com>
2021-06-03 15:01:34 +0800

    feat(SheetTable): extract <SheetTable />

--- client/HomePage/SheetData.js
+++ client/HomePage/SheetData.js
@@ -1,7 +1,7 @@
 import * as Chakra from "@chakra-ui/react";
 import * as React from "react";
 import useSWR from "swr";
-import { dateTimeFullFormat } from "../format";
+import SheetTable from "../SheetTable";
 import Loading from "./Loading";
 
 function fetcherJson(...args) {
@@ -28,39 +28,10 @@ export default function SheetData() {
     return <Loading />;
   }
   return (
-    <React.Fragment>
-      <Chakra.Heading as="h4" size="md" textAlign="right">
-        最新資料 {lastEntryDatetime}
-        <br />
-        <Chakra.Text as="i" fontSize="sm">
-          資料來源每五分鐘自動更新
-        </Chakra.Text>
-      </Chakra.Heading>
-      <Chakra.Table variant="striped">
-        <Chakra.TableCaption />
-        <Chakra.Thead>
-          {data.slice(0, 2).map((list, index) => (
-            <Chakra.Tr key={index}>
-              {list.map((text, index) => (
-                <Chakra.Th key={index}>{text}</Chakra.Th>
-              ))}
-            </Chakra.Tr>
-          ))}
-        </Chakra.Thead>
-        <Chakra.Tbody>
-          {data.slice(2).map((list, index) => (
-            <Chakra.Tr key={index}>
-              {list.map((text, index) => (
-                <Chakra.Td key={index}>{text}</Chakra.Td>
-              ))}
-            </Chakra.Tr>
-          ))}
-        </Chakra.Tbody>
-        <Chakra.Tfoot />
-      </Chakra.Table>
-      <Chakra.Box as="pre" py={4} display="none">
-        <Chakra.Code width="full">{data}</Chakra.Code>
-      </Chakra.Box>
-    </React.Fragment>
+    <SheetTable
+      lastEntryDatetime={lastEntryDatetime}
+      header={data.slice(0, 2)}
+      data={data.slice(2)}
+    />
   );
 }
---
+++ client/SheetTable.js
@@ -0,0 +1,41 @@
+import * as Chakra from "@chakra-ui/react";
+import * as React from "react";
+
+export default function SheetTable({ lastEntryDatetime, header, data }) {
+  return (
+    <React.Fragment>
+      <Chakra.Heading as="h4" size="md" textAlign="right">
+        最新資料 {lastEntryDatetime}
+        <br />
+        <Chakra.Text as="i" fontSize="sm">
+          資料來源每五分鐘自動更新
+        </Chakra.Text>
+      </Chakra.Heading>
+      <Chakra.Table variant="striped">
+        <Chakra.TableCaption />
+        <Chakra.Thead>
+          {header.map((list, index) => (
+            <Chakra.Tr key={index}>
+              {list.map((text, index) => (
+                <Chakra.Th key={index}>{text}</Chakra.Th>
+              ))}
+            </Chakra.Tr>
+          ))}
+        </Chakra.Thead>
+        <Chakra.Tbody>
+          {data.map((list, index) => (
+            <Chakra.Tr key={index}>
+              {list.map((text, index) => (
+                <Chakra.Td key={index}>{text}</Chakra.Td>
+              ))}
+            </Chakra.Tr>
+          ))}
+        </Chakra.Tbody>
+        <Chakra.Tfoot />
+      </Chakra.Table>
+      <Chakra.Box as="pre" py={4} display="none">
+        <Chakra.Code width="full">{data}</Chakra.Code>
+      </Chakra.Box>
+    </React.Fragment>
+  );
+}
