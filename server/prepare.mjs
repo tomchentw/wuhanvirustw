ef3f9a66ca96e177044a519c24f82f34dd09ad22
Tom Chen <developer@tomchentw.com>
2021-06-03 13:40:45 +0800

    feat(server/prepare): extract prepare script

--- server/prepare.mjs
+++ server/prepare.mjs
@@ -1,34 +1,20 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 import spawn from "cross-spawn";
-import crawl from "./crawl.mjs";
 import logger from "./logger.mjs";
 
-const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
-
-const COMMAND_FN_BY = {
-  crawl,
-};
-
-async function run([commandName]) {
+async function run() {
   // Prepare data folder to work with
   if (!process.env.CI) {
     spawn.sync("rm", ["-rf", "public/data"], { stdio: "inherit" });
+    spawn.sync("git", ["fetch", "--all"], { stdio: "inherit" });
   }
   spawn.sync("git", ["restore", "--source=origin/gh-pages", "data"], {
     stdio: "inherit",
   });
   spawn.sync("mv", ["data", "public/data"], { stdio: "inherit" });
-
-  const commandFn = COMMAND_FN_BY[commandName];
-  await commandFn();
 }
 
-run(process.argv.slice(2))
-  .then((it) => {
-    logger.info("Done!");
-  })
+run()
+  .then(() => {})
   .catch((error) => {
     logger.fatal(error);
   });
